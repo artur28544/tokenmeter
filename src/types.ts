636d51a21a2fed5dc874c5cd15013295fdced6a4
chu2bard <bxndnbdnd29@gmail.com>
2025-05-13 14:51:36 +0200

    simplify parser

---
+++ src/types.ts
@@ -0,0 +1,31 @@
+export interface TokenCount {
+  text: string;
+  tokens: number;
+  model: string;
+  encoding: string;
+}
+
+export interface CostEstimate {
+  provider: string;
+  model: string;
+  inputTokens: number;
+  outputTokens: number;
+  inputCost: number;
+  outputCost: number;
+  totalCost: number;
+  currency: string;
+}
+
+export interface PricingTable {
+  [model: string]: {
+    inputPerMillion: number;
+    outputPerMillion: number;
+  };
+}
+
+export interface CountResult {
+  tokens: number;
+  characters: number;
+  words: number;
+  lines: number;
+}
