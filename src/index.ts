86e7d96c1518b271539698716ecda14d600a1177
chu2bard <bxndnbdnd29@gmail.com>
2025-07-17 13:15:11 +0200

    add parser tests

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@
 export { countTokens, countTokensForFile } from "./counter.js";
 export { estimateCost, listModels } from "./pricing.js";
 export type { TokenCount, CostEstimate, CountResult, PricingTable } from "./types.js";
+
