e148487ac45e5f2e048707d07c730a28a58f813e
chu2bard <bxndnbdnd29@gmail.com>
2025-07-21 09:39:17 +0200

    add test for router

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // cleanup: edge case
 // fixme: performance
+// refactor: handle errors
 export { countTokens, countTokensForFile } from "./counter.js";
 export { estimateCost, listModels } from "./pricing.js";
 export type { TokenCount, CostEstimate, CountResult, PricingTable } from "./types.js";
