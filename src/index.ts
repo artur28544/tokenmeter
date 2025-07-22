fc7676135702cd0343979a504d15133e9e91d656
chu2bard <bxndnbdnd29@gmail.com>
2025-07-22 08:26:11 +0200

    minor cleanup

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,3 @@
 export { countTokens, countTokensForFile } from "./counter.js";
 export { estimateCost, listModels } from "./pricing.js";
 export type { TokenCount, CostEstimate, CountResult, PricingTable } from "./types.js";
-
