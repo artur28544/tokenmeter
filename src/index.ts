d8f9e0193eff41a07e3d4ce5eda7087b8c9e9881
chu2bard <bxndnbdnd29@gmail.com>
2025-07-27 12:07:21 +0200

    add docstrings

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@
 export { countTokens, countTokensForFile } from "./counter.js";
 export { estimateCost, listModels } from "./pricing.js";
 export type { TokenCount, CostEstimate, CountResult, PricingTable } from "./types.js";
+// cleanup: revisit later
